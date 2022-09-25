205ef5ae94059e5e16b6ef09e2a68a0d9d9e7ab8
Mate Pejnovac <85074903+penmate@users.noreply.github.com>
2022-09-25 14:40:33 +0200

    Delete connect.php

--- global/connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$user = 'root';
-$pw = '';
-$database = 'advdb';
-
-$db = new mysqli('localhost', $user, $pw, $database) or die('Unable to connect to the Host!');
-print_r('Successfully connected to the host!');
